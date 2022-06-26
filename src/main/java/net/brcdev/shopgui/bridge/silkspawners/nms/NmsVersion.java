fb06d001d6eeaebc231df9fec4c1e665468c3d43
Emil Matyjaszewski <brc@brcdev.net>
2022-06-26 23:01:07 +0200

    Update to Spigot 1.19

--- src/main/java/net/brcdev/shopgui/bridge/silkspawners/nms/NmsVersion.java
+++ src/main/java/net/brcdev/shopgui/bridge/silkspawners/nms/NmsVersion.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public enum NmsVersion {
 
-  v1_7, v1_8, v1_9, v1_10, v1_11, v1_12, v1_13, v1_14, v1_15, v1_16, v1_17, v1_18;
+  v1_7, v1_8, v1_9, v1_10, v1_11, v1_12, v1_13, v1_14, v1_15, v1_16, v1_17, v1_18, v1_19;
 
   private static final Pattern ONE_POINT_VERSION_NUMBER_PATTERN;
 
