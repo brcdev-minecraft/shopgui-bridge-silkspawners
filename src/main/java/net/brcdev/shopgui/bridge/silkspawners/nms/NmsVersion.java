34d0d1c4297772f4a41f19a683f9f4ac8622c884
Mackenzie Molloy <mackenzie.j.molloy@gmail.com>
2024-06-09 01:45:11 +0100

    Added support for 1.20.5/6

--- src/main/java/net/brcdev/shopgui/bridge/silkspawners/nms/NmsVersion.java
+++ src/main/java/net/brcdev/shopgui/bridge/silkspawners/nms/NmsVersion.java
@@ -6,20 +6,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum NmsVersion {
+  v1_7,
+  v1_8,
+  v1_9,
+  v1_10,
+  v1_11,
+  v1_12,
+  v1_13,
+  v1_14,
+  v1_15,
+  v1_16,
+  v1_17,
+  v1_18,
+  v1_19,
+  v1_20;
 
-  v1_7, v1_8, v1_9, v1_10, v1_11, v1_12, v1_13, v1_14, v1_15, v1_16, v1_17, v1_18, v1_19, v1_20;
-
-  private static final Pattern ONE_POINT_VERSION_NUMBER_PATTERN;
-
-  static {
-    ONE_POINT_VERSION_NUMBER_PATTERN = Pattern.compile("v\\d_(\\d+)");
-  }
+  private static final Pattern ONE_POINT_VERSION_NUMBER_PATTERN = Pattern.compile("v\\d_(\\d+)");
 
   public int extractVersionNumber() {
-    Matcher matcher = ONE_POINT_VERSION_NUMBER_PATTERN.matcher(name());
+    Matcher matcher = ONE_POINT_VERSION_NUMBER_PATTERN.matcher(this.name());
     matcher.find();
-    if (matcher.groupCount() < 1)
+
+    if (matcher.groupCount() < 1) {
       throw new UnsupportedMinecraftVersionException();
-    return Integer.valueOf(matcher.group(1)).intValue();
+    }
+
+    return Integer.valueOf(matcher.group(1));
   }
 }
