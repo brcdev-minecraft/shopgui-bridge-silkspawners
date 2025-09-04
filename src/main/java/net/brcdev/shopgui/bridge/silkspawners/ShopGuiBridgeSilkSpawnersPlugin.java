eaff15fecea014186e5e4b4b94fdffc63a6684f9
R00tB33rMan <rootb33rman@gmail.com>
2025-09-04 14:46:58 -0400

    Rebase

--- src/main/java/net/brcdev/shopgui/bridge/silkspawners/ShopGuiBridgeSilkSpawnersPlugin.java
+++ src/main/java/net/brcdev/shopgui/bridge/silkspawners/ShopGuiBridgeSilkSpawnersPlugin.java
@@ -2,11 +2,8 @@ package net.brcdev.shopgui.bridge.silkspawners;
 
 import me.nahu.scheduler.wrapper.FoliaWrappedJavaPlugin;
 import net.brcdev.shopgui.ShopGuiPlusApi;
-import net.brcdev.shopgui.bridge.silkspawners.spawner.SilkSpawnersCurrentProvider;
-import net.brcdev.shopgui.bridge.silkspawners.spawner.SilkSpawnersLegacyProvider;
 import net.brcdev.shopgui.bridge.silkspawners.spawner.SilkSpawnersProvider;
 import net.brcdev.shopgui.exception.api.ExternalSpawnerProviderNameConflictException;
-import org.bukkit.Bukkit;
 
 public class ShopGuiBridgeSilkSpawnersPlugin extends FoliaWrappedJavaPlugin {
 
@@ -18,12 +15,8 @@ public class ShopGuiBridgeSilkSpawnersPlugin extends FoliaWrappedJavaPlugin {
   }
 
   private void hookIntoSilkSpawners() {
-    if (usingLegacySilkSpawners()) {
-      this.spawnerProvider = new SilkSpawnersLegacyProvider();
-    } else {
-      this.spawnerProvider = new SilkSpawnersCurrentProvider();
-    }
-    this.spawnerProvider.hookIntoSilkSpawners(Bukkit.getPluginManager().getPlugin("SilkSpawners"));
+    this.spawnerProvider = new SilkSpawnersProvider();
+    this.spawnerProvider.hookIntoSilkSpawners();
   }
 
   private void hookIntoShopGui() {
@@ -33,16 +26,4 @@ public class ShopGuiBridgeSilkSpawnersPlugin extends FoliaWrappedJavaPlugin {
       getLogger().warning("Failed to hook into ShopGUI+: " + e.getMessage());
     }
   }
-
-  private boolean usingLegacySilkSpawners() {
-    int versionMajorNumber;
-    String version = Bukkit.getPluginManager().getPlugin("SilkSpawners").getDescription().getVersion();
-    try {
-      versionMajorNumber = Integer.parseInt(String.valueOf(version.charAt(0)));
-    } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
-      return true;
-    }
-    return (versionMajorNumber < 6);
-  }
 }
-
